d98df54aadd0ca9efd81495729a0a7313d113811
RaviYadav3003 <114741865+RaviYadav3003@users.noreply.github.com>
2022-10-13 20:02:42 +0530

    fix : quantity bug fix

--- script.js
+++ script.js
@@ -5,28 +5,32 @@ let btnVlaue = document.querySelector(".btn");
 let ResultValue = document.querySelector(".result");
 
 function onBtnClicked() {
-  let initialPriceValue = Number.parseInt(initialPrice.value);
-  let qtyStockValue = Number.parseInt(qtyStock.value);
-  let currentPriceValue = Number.parseInt(currentPrice.value);
+  let initialPriceValue = Number(initialPrice.value);
+  let qtyStockValue = Number(qtyStock.value);
+  let currentPriceValue = Number(currentPrice.value);
   //   console.log(initialPriceValue);
   //   console.log(qtyStockValue);
   //   console.log(currentPriceValue);
-  
-  if (initialPriceValue > currentPriceValue) {
+  if(initialPriceValue== 0 || qtyStockValue == 0 ){
+    resultToPrint = "Enter vaild number";
+  }
+  else if (initialPriceValue > currentPriceValue) {
     console.log("if");
     var lossValue = initialPriceValue - currentPriceValue;
     var lossTotal = lossValue * qtyStockValue;
-    var lossPercentage = (lossValue / initialPriceValue) * 100;
+    var lossPercentage = (lossTotal / initialPriceValue) * 100;
     var resultToPrint = `Your Loss is ${lossTotal} & Loss Percent is ${lossPercentage}%`;
     // console.log(ResultValue);
   } else if (currentPriceValue > initialPriceValue) {
     // console.log("elseif");
     var profitValue = currentPriceValue - initialPriceValue;
     var profitTotal = profitValue * qtyStockValue;
-    var profitPercentage = (profitValue / currentPriceValue) * 100;
+    var profitPercentage = (profitTotal / initialPriceValue) * 100;
 
     resultToPrint = `Your Profit is ${profitTotal} & Profit Percent is ${profitPercentage}%`;
     console.log(ResultValue);
+  } else if(initialPriceValue == currentPriceValue) {
+    resultToPrint = "Wait for market to open";
   }else {
     resultToPrint = "Enter vaild number";
   }
